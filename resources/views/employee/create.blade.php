8db8610cb73d13e5f56098e30e77a5c4697ce9b2
traderjeffb <traderjeffb@gmail.com>
2021-07-06 12:41:41 -0400

    added bootstrap to create view

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -4,20 +4,20 @@
 @section('content')
 <div class="">
   <div class="d-flex">
-    <h3 class="mx-auto">New Employee Infomation</h3>
+    <h3 class="mx-auto mt-4">New Employee Infomation</h3>
   </div>
   <div class="form d-flex ms-2">
-    <form class="form-group form-control-md col-md-4 border p-1  mx-auto  bg-light"  action="{{ url('store') }}" method="post" id="add-employee-form">
+    <form class="form-group form-control-md col-md-4 border p-1  mx-auto  bg-light font-weight-bold "  action="{{ url('store') }}" method="post" id="add-employee-form">
       @csrf
-            <label for="name">Name</label>
+            <label for="name" class="ml-md-2">Name</label>
             <input class="input-group" name="name"><br>
-            <label for="address">Address</label>
+            <label for="address" class="ml-md-2">Address</label>
             <input class="input-group"  name="address"><br>
-            <label for="city">City</label>
+            <label for="city" class="ml-md-2">City</label>
             <input  class="input-group" name="city"><br>
-            <label for="state">State</label>
+            <label for="state" class="ml-md-2">State</label>
             <input  class="input-group" name="state"><br>
-            <label for="zipcode">Zipcode</label>
+            <label for="zipcode" class="ml-md-2">Zipcode</label>
             <input  class="input-group" name="zipcode">
             <button class= "m-2 rounded" type="submit">Submit</button>
     </form>
