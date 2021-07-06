5489307f80d7c226387588a77d104c92a3975129
traderjeffb <traderjeffb@gmail.com>
2021-07-06 12:42:55 -0400

    added jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,8 @@
     <!-- Styles -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Human Resources</a>
@@ -41,3 +43,8 @@
   @yield('content')
 </body>
 </html>
+<Style>
+  setTimeout(function() {
+    $('#successMessage').fadeOut('fast');
+  }, 30000);
+  </Style>
