f7ee52682e764049bb47cfbd409bedc35fa37aec
traderjeffb <traderjeffb@gmail.com>
2021-07-08 09:40:04 -0400

    fixed edit view

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -1,6 +1,7 @@
 
 @extends('layouts.app');
 @section('content')
+
 <div class="d-flex">  
   <h3 class="mx-auto mt-4" >Editing : {{ $employee['name'] }} </h3>
 </div>
@@ -21,20 +22,4 @@
     </form>
   </div>   
 </div>
-{{-- 
-<form class="" action="{{ url('update/'.$employee['id'])}}" method="post" id="edit-employee-form">
-  @csrf
-        <label for="name">Name</label>
-        <input name="name" value={{ $employee['name'] }}><br>
-        <label for="address">Address</label>
-        <input name="address" value={{ $employee['address'] }}><br>
-        <label for="city">City</label>
-        <input name="city" value={{ $employee['city'] }}><br>
-        <label for="state">State</label>
-        <input name="state" value={{ $employee['state'] }}><br>
-        <label for="zipcode">zipcode</label>
-        <input name="zipcode" value={{ $employee['zipcode'] }}>
-        <button type="submit">Submit</button>
-</form> --}}
-</div>   
 @endsection
