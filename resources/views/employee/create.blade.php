312dc1c66b7e21cead6c32e92e6ffca4881308d6
traderjeffb <traderjeffb@gmail.com>
2021-07-08 11:49:15 -0400

    updated create view

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -10,15 +10,31 @@
     <form class="form-group form-control-md col-md-4 border p-1  mx-auto  bg-light font-weight-bold "  action="{{ url('store') }}" method="post" id="add-employee-form">
       @csrf
             <label for="name" class="ml-md-2">Name</label>
-            <input class="input-group" name="name"><br>
+            <input class="input-group" name="name" required><br>
             <label for="address" class="ml-md-2">Address</label>
-            <input class="input-group"  name="address"><br>
+            <input class="input-group"  name="address" required><br>
             <label for="city" class="ml-md-2">City</label>
-            <input  class="input-group" name="city"><br>
+            <input  class="input-group" name="city" required><br>
             <label for="state" class="ml-md-2">State</label>
-            <input  class="input-group" name="state"><br>
+            <input  class="input-group" name="state" required><br>
             <label for="zipcode" class="ml-md-2">Zipcode</label>
-            <input  class="input-group" name="zipcode">
+            <input  class="input-group" name="zipcode" required>
+
+            <input type="radio" id="fullTime" name="employment_status" value="FullTime">
+            <label for="fullTime">Full Time</label><br>
+            <input type="radio" id="partTime" name="employment_status" value="PartTime">
+            <label for="partTime">Part Time</label><br>
+            <input type="radio" id="tempHelp" name="employment_status" value="TempHelp">
+            <label for="tempHelp">Temporary Help</label><br>
+            <div>
+              <label for="gender">Gender:</label>
+              <select id="gender" name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="LBGTQ">LBGTQ</option>
+              </select>
+            </div>
+
             <button class= "m-2 rounded" type="submit">Submit</button>
     </form>
   </div>   
