c7f33bfbf20c13d4144d8f0d961ee816406ab327
traderjeffb <traderjeffb@gmail.com>
2021-07-10 01:25:48 -0400

    added a migration

---
+++ database/migrations/2021_07_10_020021_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->text('firstName');
+            $table->text('lastName');
+            $table->text('address');
+            $table->text('city');
+            $table->text('state');
+            $table->integer('zipcode');
+            $table->string('email');
+            $table->integer('customerRating');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
