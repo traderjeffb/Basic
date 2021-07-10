d1f81cda29b14d157836117b72912ad2c27c0140
traderjeffb <traderjeffb@gmail.com>
2021-07-10 01:24:43 -0400

    added a model

---
+++ app/Models/Customer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    //protected $table = 'customer';
+
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'email',
+        'customerRating',
+    ];
+}
