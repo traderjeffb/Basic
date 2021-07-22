ef8e3d7d2395115816918bcbeb06e4625b3a0920
traderjeffb <traderjeffb@gmail.com>
2021-07-21 23:25:06 -0400

    added jqery to app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,16 +14,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Scripts -->
-    {{--  <script src="{{ asset('js/app.js') }}" defer></script>  --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    {{-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
 
       <!-- Required meta tags -->
   <meta charset="utf-8">
@@ -42,11 +38,11 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active font-weight-bold" href= {{ route('employee.index') }}>Employees <span class="sr-only">(current)</span></a>
+      <a class="p-2 nav-item nav-link active font-weight-bold" href= {{ route('employee.index') }}>Employees <span class="sr-only">(current)</span></a>
       {{--  <a class="nav-item nav-link" href= {{ route('create') }}>Add Employee</a>  --}}
-      <a class="nav-item nav-link active font-weight-bold" href= {{ route('customer.index') }}>Customers <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link active font-weight-bold" href= {{ route('product.indexInternal') }}>Products <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link active font-weight-bold" href= "#">Sales<span class="sr-only">(current)</span></a>
+      <a class="p-2 nav-item nav-link active font-weight-bold" href= {{ route('customer.index') }}>Customers <span class="sr-only">(current)</span></a>
+      <a class="p-2 nav-item nav-link active font-weight-bold" href= {{ route('product.indexInternal') }}>Products <span class="sr-only">(current)</span></a>
+      <a class="p-2 nav-item nav-link active font-weight-bold" href= "#">Customer Sales<span class="sr-only">(current)</span></a>
     </div>
   </div>
 </nav>
